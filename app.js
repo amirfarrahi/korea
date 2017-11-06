a76fd341cfe0357aa7c58842a31f6d369d202275
Amir <amirfarrahi@Amirs-MacBook-Air.local>
2017-11-05 19:13:47 -0500

    adding server

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/radar/:id', function (req,res) {
 
 	var id = req.params.id;
 
-connection.connect();
+//connection.connect();
  
 
 	connection.query('select s.id,s.time,s.flow,s.density,IFNULL(c.lat,0) accident_ind,IFNULL(c.response_time,-1) response_time ,IFNULL(c.turn_around_time,-1) turn_around_time from speed_vol_agg s left outer join collisions c on (c.date=s.date and s.id=c.radar_id and c.time=s.time) where s.id=? order by 1,2', [id], function(err, rows, fields) {
@@ -53,7 +53,7 @@ connection.connect();
 			} else {
 				response.push({'result' : 'error', 'msg' : 'No Results Found'});
 			}
-                        connection.end();
+        //                connection.end();
 			res.setHeader('Content-Type', 'application/json');
                         
 	    	res.status(200).send(JSON.stringify(response));
